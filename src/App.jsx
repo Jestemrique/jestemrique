85dfb322d72190b9053f57dcc98845751c37aed3
Jestemrique <enrique.pastrana@gmail.com>
2025-10-08 17:48:34 +0200

    Started css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { Route, Routes, useLocation } from 'react-router-dom';
 import { StoryblokComponent, useStoryblok } from '@storyblok/react';
 import NavBar from './components/NavBar.jsx';
-
+import './App.css';
 
 function App() {
-
   const location = useLocation();
   let slug = location.pathname === '/' ? 'home' : location.pathname.replace("/", "");
 
@@ -12,18 +11,69 @@ function App() {
   const contentStory = useStoryblok(slug, { version: 'draft' });
   const notFoundStory = useStoryblok('not-found', { version: 'draft' });
 
-  if (!navStory.content) return <div>Loading navbar...</div>;
+  if (!navStory?.content) return <div>Loading navbar...</div>;
 
   const pageToShow = contentStory?.content ? contentStory : notFoundStory;
-
   if (!pageToShow?.content) return <div>Page not found...</div>;
 
   return (
     <>
-      <NavBar blok={navStory?.content} />
-      <StoryblokComponent blok={pageToShow.content} />
+    <div className="container">
+      <div className="columns">
+        <div className="column is-one-fifth-tablet navbar-column ">
+          <NavBar blok={navStory.content} />
+        </div>
+        <div className="column main-column green-border">
+          <StoryblokComponent blok={pageToShow.content} />
+        </div>
+      </div>
+    </div>
     </>
   );
 }
 
-export default App
+export default App;
+
+
+
+
+
+
+
+
+
+
+// import { Route, Routes, useLocation } from 'react-router-dom';
+// import { StoryblokComponent, useStoryblok } from '@storyblok/react';
+// import NavBar from './components/NavBar.jsx';
+
+// // import './App.css';
+
+
+// function App() {
+
+//   const location = useLocation();
+//   let slug = location.pathname === '/' ? 'home' : location.pathname.replace("/", "");
+
+//   const navStory = useStoryblok('main-navbar', { version: 'draft' });
+//   const contentStory = useStoryblok(slug, { version: 'draft' });
+//   const notFoundStory = useStoryblok('not-found', { version: 'draft' });
+
+//   if (!navStory.content) return <div>Loading navbar...</div>;
+
+//   const pageToShow = contentStory?.content ? contentStory : notFoundStory;
+
+//   if (!pageToShow?.content) return <div>Page not found...</div>;
+
+//   return (
+//     <>
+//       <div className="container">
+//       <NavBar blok={navStory?.content} />
+//       <StoryblokComponent blok={pageToShow.content} />
+//       </div>
+
+//     </>
+//   );
+// }
+
+// export default App
