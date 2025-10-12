e47e8289d8c063a7f06112f3c719a0ac7a4e473b
Jestemrique <enrique.pastrana@gmail.com>
2025-10-12 17:52:17 +0200

    fitler album query for bands

--- src/components/Band.jsx
+++ src/components/Band.jsx
@@ -1,9 +1,16 @@
 import { StoryblokComponent } from "@storyblok/react";
 import LastFmBand from "./LastFmBand";
+import ListAlbums from "./ListAlbums";
+import { useLocation } from "react-router-dom";
 
 const Band = ({ blok }) => {
+    console.log("band blok: ", blok);
+    const { state } = useLocation();
+    const bandUuid = state?.uuid ?? blok?.uuid; 
+
     return (
         <>
+        <h1>band name: {blok.name}</h1>
             <h1 className="oswald-font is-size-4-mobile is-size-3-tablet is-underlined ">{blok.name}</h1>
 
             <div className="content">
@@ -14,7 +21,10 @@ const Band = ({ blok }) => {
             </div>
 
             <LastFmBand band={blok.name} />
-
+            <h1>
+            {blok.name}</h1>
+            <ListAlbums bandUuid={bandUuid} />
+            {/* <ListAlbums bandUuid={blok.uuid} /> */}
         </>
     )
 };
